49b44713608b30881fa144dd14e417b61f359bdc
Barnaby <accounts@southcla.ws>
2022-10-24 16:54:01 +0100

    progress

--- fault.go
+++ fault.go
@@ -2,10 +2,12 @@ package fault
 
 // New creates a new basic fault error.
 func New(message string) error {
+	stack := callers(3)
 	return &fault{
-		root:  true,
-		msg:   message,
-		stack: callers(3),
+		root:   true,
+		msg:    message,
+		stack:  stack,
+		global: stack.isGlobal(),
 	}
 }
 
@@ -13,21 +15,85 @@ type wrapper func(err error) error
 
 // Wrap wraps an error with all of the wrappers provided.
 func Wrap(err error, w ...wrapper) error {
+	if err == nil {
+		return nil
+	}
+
+	// callers(4) skips runtime.Callers, stack.callers, this method, and Wrap(f)
+	stack := callers(4)
+	// caller(3) skips stack.caller, this method, and Wrap(f)
+	// caller(skip) has a slightly different meaning which is why it's not 4 as above
+	frame := caller(3)
+	switch e := err.(type) {
+	case *fault:
+		if e.root {
+			if e.global {
+				// create a new root error for global values to make sure nothing interferes with the stack
+				err = &fault{
+					global: e.global,
+					stack:  stack,
+				}
+			} else {
+				// insert the frame into the stack
+				e.stack.insertPC(*stack)
+			}
+		} else {
+			if root, ok := Cause(err).(*fault); ok {
+				root.stack.insertPC(*stack)
+			}
+		}
+	default:
+		// return a new root error that wraps the external error
+		return &fault{
+			cause: e,
+			stack: stack,
+		}
+	}
+
+	// run all the decorators after the stack info is figured out.
 	for _, fn := range w {
 		err = fn(err)
 	}
+
 	return &fault{
 		root:  false,
 		cause: err,
 		stack: callers(3),
+		frame: frame,
 	}
 }
 
+// Cause returns the root cause of the error, which is defined as the first error in the chain. The original
+// error is returned if it does not implement `Unwrap() error` and nil is returned if the error is nil.
+func Cause(err error) error {
+	for {
+		uerr := Unwrap(err)
+		if uerr == nil {
+			return err
+		}
+		err = uerr
+	}
+}
+
+// Unwrap returns the result of calling the Unwrap method on err, if err's type contains an Unwrap method
+// returning error. Otherwise, Unwrap returns nil.
+func Unwrap(err error) error {
+	u, ok := err.(interface {
+		Unwrap() error
+	})
+	if !ok {
+		return nil
+	}
+	return u.Unwrap()
+}
+
 type fault struct {
-	root  bool   // is this error the first in the chain?
-	msg   string // root error message
-	cause error  // nil if root == true, otherwise, the wrapped error
-	stack *stack // stack pc
+	root   bool   // is this error the first in the chain?
+	global bool   // is this a globally declared sentinel error?
+	msg    string // root error message
+	cause  error  // if this wraps another error
+	stack  *stack // the full stack trace of a root error
+	frame  *frame // the stack frame of a wrapped error
 }
 
 func (f *fault) Error() string {
@@ -37,6 +103,7 @@ func (f *fault) Error() string {
 		return f.cause.Error()
 	}
 }
+
 func (f *fault) Unwrap() error { return f.cause }
 
 func (f *fault) Stack() Stack {
--- fault_test.go
+++ fault_test.go
@@ -1,18 +1,63 @@
 package fault_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/Southclaws/fault"
 	"github.com/kr/pretty"
 )
 
+var (
+	errSentinelStdlib = errors.New("stdlib sentinel error")
+	errSentinelFault  = fault.New("fault sentinel error")
+)
+
+func rootCause(kind int) error {
+	if kind == 0 {
+		return nil
+	} else if kind == 1 {
+		return errSentinelStdlib
+	} else if kind == 2 {
+		return errSentinelFault
+	} else if kind == 3 {
+		return errors.New("stdlib root cause error")
+	} else {
+		return fault.New("fault root cause error")
+	}
+}
+
+func errorCallerDeep(kind int) error {
+	err := rootCause(kind)
+	if err != nil {
+		return fault.Wrap(err)
+	}
+
+	return nil
+}
+
+func errorCallerMid(kind int) error {
+	err := errorCallerDeep(kind)
+	if err != nil {
+		return fault.Wrap(err)
+	}
+
+	return nil
+}
+
+func errorCaller(kind int) error {
+	err := errorCallerMid(kind)
+	if err != nil {
+		return fault.Wrap(err)
+	}
+
+	return nil
+}
+
 func Test_fault_Format(t *testing.T) {
-	err := fault.New("root cause")
-	err = fault.Wrap(err, fault.Msg("wrapped"))
-	err = fault.Wrap(err, fault.Msg("wrapped"))
+	err := errorCaller(1)
 
 	f := err.(interface{ Stack() fault.Stack })
 
-	pretty.Printf("%+v\n", f.Stack().String())
+	pretty.Printf("%+v\n", f.Stack())
 }
