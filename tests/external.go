11fcd8bfca6098101f2d9d9190ce3b1cccf41814
Barnaby <accounts@southcla.ws>
2022-11-29 16:43:20 +0000

    bug repro with pg error

--- tests/external.go
+++ tests/external.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/jackc/pgconn"
 	pkg_errors "github.com/pkg/errors"
 )
 
@@ -18,4 +19,31 @@ func externalError() error {
 
 func externalWrappedError() error {
 	return pkg_errors.Wrap(errExternalPkgerr, "external error wrapped with pkg/errors")
+}
+
+func externalWrappedPostgresError() error {
+	err := pgerror()
+	return fmt.Errorf("external pg error: %w", err)
+}
+
+func pgerror() error {
+	return &pgconn.PgError{
+		Severity:         "fatal",
+		Code:             "123",
+		Message:          "your sql was wrong bro",
+		Detail:           "very wrong",
+		Hint:             "rtfm",
+		Position:         420,
+		InternalPosition: 69,
+		InternalQuery:    "internal_query",
+		Where:            "where",
+		SchemaName:       "schema_name",
+		TableName:        "table_name",
+		ColumnName:       "column_name",
+		DataTypeName:     "data_type_name",
+		ConstraintName:   "constraint_name",
+		File:             "file",
+		Line:             0,
+		Routine:          "routine",
+	}
 }
